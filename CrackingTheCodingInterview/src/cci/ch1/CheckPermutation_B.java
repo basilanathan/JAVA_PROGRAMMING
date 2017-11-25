729df6b84ec160d3ebe3d80d1b755ca02cd5225f
basila1 <basila_n@outlook.com>
2017-11-24 16:46:41 -0800

    1.2

---
+++ CrackingTheCodingInterview/src/cci/ch1/CheckPermutation_B.java
@@ -0,0 +1,43 @@
+package cci.ch1;
+
+/**
+ * 
+ * @author basila
+ * @date 11/24/2017
+ * 
+ * Time O(N)
+ */
+
+public class CheckPermutation_B {
+	//driver method
+	public static void main(String[] args) {
+		
+		String [][] pairs = {{"apple", "papel"}, {"carrot", "tarroc"}, {"hello", "ldeoh"}};
+		for(String[] pair : pairs) {
+			String word1 = pair[0];
+			String word2 = pair[1];
+			
+			boolean perms = permutation(word1, word2);
+			System.out.println(word1 + ", " + word2 + ": " + perms);
+		}
+		
+	}
+	
+	public static boolean permutation(String s, String t) {
+		if(s.length() != t.length()) return false;
+		
+		int[] letters = new int[128];
+		for (int i = 0; i < s.length(); i++) {
+			letters[s.charAt(i)]++; //go through all the characters and incremenent each value of array to one
+		}
+		
+		for(int i = 0; i < t.length(); i++) {
+			letters[t.charAt(i)]--; //go through all the characters decrement id the value is allready there
+			if(letters[t.charAt(i)] < 0) { //if there are negative numbers then then string contains values that are not present in the first string
+				return false; //contains a letter not in the first string
+			}
+		}
+		return true; //letters array contains no negative values
+	}
+
+}
--- CrackingTheCodingInterview/src/cci/ch1/isUnique.java
+++ CrackingTheCodingInterview/src/cci/ch1/isUnique.java
@@ -1,4 +1,4 @@
-package ch1.isUnique;
+package cci.ch1;
 
 /**
  * @author basila nathan
