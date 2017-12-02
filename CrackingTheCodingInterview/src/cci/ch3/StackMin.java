867b44a9090f38b9a48c9a1dc9125484273216f2
basila1 <basila_n@outlook.com>
2017-12-02 14:56:40 -0800

    3.4 queue via stacks

--- CrackingTheCodingInterview/src/cci/ch3/StackMin.java
+++ CrackingTheCodingInterview/src/cci/ch3/StackMin.java
@@ -31,34 +31,34 @@ public class StackMin extends Stack<NodeWithMin> {
 }
 
 //using an additional stack which keeps track of the mins
-public class StackWithMin2 extends Stack<Integer> {
-	Stack<Integer> s2;
-	
-	public StackWithMin2() {
-		s2 = new Stack<Integer>();
-	}
-	
-	public void push(int value) {
-		if(value <= min) {
-			s2.push(value);
-		}
-		super.push(value);
-	}
-	
-	public Integer pop() {
-		int value = super.pop();
-		if(value == min) {
-			s2.pop();
-		}
-		return value;
-	}
-	
-	public int min() {
-		if(s2.isEmpty()) {
-			return Integer.MAX_VALUE;
-		} else {
-			return s2.peek();
-		}
-	}
-}
+//public class StackWithMin2 extends Stack<Integer> {
+//	Stack<Integer> s2;
+//	
+//	public StackWithMin2() {
+//		s2 = new Stack<Integer>();
+//	}
+//	
+//	public void push(int value) {
+//		if(value <= min) {
+//			s2.push(value);
+//		}
+//		super.push(value);
+//	}
+//	
+//	public Integer pop() {
+//		int value = super.pop();
+//		if(value == min) {
+//			s2.pop();
+//		}
+//		return value;
+//	}
+//	
+//	public int min() {
+//		if(s2.isEmpty()) {
+//			return Integer.MAX_VALUE;
+//		} else {
+//			return s2.peek();
+//		}
+//	}
+//}
 
--- CrackingTheCodingInterview/src/cci/ch3/StackOfPlates.java
+++ CrackingTheCodingInterview/src/cci/ch3/StackOfPlates.java
@@ -63,5 +63,16 @@ public class StackOfPlates {
 		return removed_item;
 	}
 	
+	public static void main(String[] args) {
+		int capacity_per_substack = 5;
+		StackOfPlates set = new StackOfPlates(capacity_per_substack);
+		for (int i = 0; i < 34; i++) {
+			set.push(i);
+		}
+		for (int i = 0; i < 34; i++) {
+			System.out.println("Popped " + set.pop());
+		}		
+	}
+	
 
 }
