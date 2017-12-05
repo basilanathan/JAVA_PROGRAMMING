0f05d846d28ec61204a4d5e51ef435b65e296e96
basila1 <basila_n@outlook.com>
2017-12-04 20:49:02 -0800

    refactor 4.3 BFS

--- CrackingTheCodingInterview/src/cci/ch4/ListOfDepths.java
+++ CrackingTheCodingInterview/src/cci/ch4/ListOfDepths.java
@@ -6,6 +6,19 @@ import java.util.LinkedList;
 
 import helpers.TreeNode;
 
+/**
+ * 
+ * @author basila
+ * 
+ * Given a binary tree, design an algorithm which 
+ * creates a linked list of all the nodes at each depth.
+ * Ex. If you have a tree with depth D, you will have
+ * D linked lists.
+ * 
+ * Time: O(N)
+ *
+ */
+
 public class ListOfDepths {
 	//method to create a linked list at each depth
 	
