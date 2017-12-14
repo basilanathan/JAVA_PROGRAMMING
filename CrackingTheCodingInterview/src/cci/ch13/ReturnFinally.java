025520c038666535fe13b0541473d26627f1fea5
basila1 <basila_n@outlook.com>
2017-12-14 14:50:43 -0800

    13.2 Return From Finally

---
+++ CrackingTheCodingInterview/src/cci/ch13/ReturnFinally.java
@@ -0,0 +1,41 @@
+package cci.ch13;
+
+/**
+ * 
+ * @author basila
+ * 
+ * <br> Problem Statement:
+ * 
+ * In Java, does the finally block gets executed if we insert
+ * a return statement inside the try block of a try-catch-finally?
+ * 
+ * </br>
+ *
+ */
+
+public class ReturnFinally {
+	
+	/* Yes, finally block will always be executed even if try block has,
+	 * 	1. return statement
+	 * 	2. exception
+	 * 	3. break statement 
+	 * 	4. continue statement
+	 *  
+	 * There are only 2 possible ways where a finally block may not get executed. They are, 
+	 * 	1. If the child thread processing the try block dies.
+	 * 	2. if the VM executing the process, gets killed */
+	
+	public static void main(String[] args) {
+		int x = 0;
+		int y = 7;
+		try {
+			int result = y/x;
+			System.out.println("Divison success !!. Result : " + result);
+		} catch (Exception exception) {
+			System.out.println("Exception occured. Message : " + exception.getMessage());
+		} finally {
+			System.out.println("Finally block executed even when there was a exception in try block !!");
+		}
+	}
+
+}
